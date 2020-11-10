2bbae50932e0f6c24d9acf2d85578f80e8a94fa5
eli17048 <andrea.elias16@gmail.com>
2020-11-10 02:29:15 -0600

    limpiando codigo

--- Luces.py
+++ Luces.py
@@ -13,14 +13,14 @@ class Luces(object):
         self.center = center
         self.material = material
 
-    def ray_intersect(self, origin, direction):
-        a = direction[0] ** 2 + direction[2] ** 2
+    def ray_intersect(self, origin, direccion):
+        a = direccion[0] ** 2 + direccion[2] ** 2
         if abs(a) < 0.0001: #epsilon = 0.0001
             return None
 
         b = 2 * (
-            direction[0] * (origin[0] - self.center[0])
-            + direction[2] * (origin[2] - self.center[2])
+            direccion[0] * (origin[0] - self.center[0])
+            + direccion[2] * (origin[2] - self.center[2])
         )
         c = (
             (origin[0] - self.center[0]) ** 2
@@ -39,55 +39,53 @@ class Luces(object):
         if t0 > t1:
             t0, t1, t1, t0
 
-        y0 = origin[1] + t0 * direction[1]
+        y0 = origin[1] + t0 * direccion[1]
         if self.center[1] < y0 and y0 <= (self.center[1] + self.altura):
-            hit = sum(origin, mul(direction,t0))
+            hit = sum(origin, mul(direccion,t0))
             normal = norm(sub(self.center, hit))
             return Intersect(distance = t0,
                              point = hit,
                              normal = normal
                              )
 
-        y1 = origin[1] + t1 * direction[1]
+        y1 = origin[1] + t1 * direccion[1]
         if self.center[1] < y1 and y1 <= (self.center[1] + self.altura):
-            hit = sum(origin, mul(direction, t1))
+            hit = sum(origin, mul(direccion, t1))
             normal = norm(sub(self.center, hit))
             return Intersect(distance=t1,
                              point = hit,
                              normal = normal
             )
-        return self.intersect_caps(origin, direction)
+        return self.intersectC(origin, direccion)
 
-    def check_cap(self, origin, direction, t):
-        x = origin[0] + t * direction[0]
-        z = origin[2] + t * direction[2]
+    def capasVerif(self, origin, direccion, t):
+        x = origin[0] + t * direccion[0]
+        z = origin[2] + t * direccion[2]
         return (x ** 2 + z ** 2) <= abs(self.radio)
 
-    def intersect_caps(self, origin, direction):
-        if self.closed == False or abs(direction[1]) < 0.0001: #epsilon = 0.0001
+    def intersectC(self, origin, direccion):
+        if self.closed == False or abs(direccion[1]) < 0.0001: #epsilon = 0.0001
             return None
 
-        t_inferior = (self.center[1] - origin[1]) / direction[1]
-        if self.check_cap(origin, direction, t_inferior):
-            hit = sum(origin, mul(direction, t_inferior))
+        t_inferior = (self.center[1] - origin[1]) / direccion[1]
+        if self.capasVerif(origin, direccion, t_inferior):
+            hit = sum(origin, mul(direccion, t_inferior))
             normal = norm(sub(hit, self.center))
-
             return Intersect(distance = t_inferior,
                              point = hit,
                              normal = normal,
-                             texCoords = None,
-                             sceneObject = self)
-
-        t_superior = ((self.center[1] + self.altura) - origin[1]) / direction[1]
-        if self.check_cap(origin, direction, t_superior):
-            hit = sum(origin, mul(direction, t_superior))
+                             Coordenadas = None,
+                             scene = self)
+        t_superior = ((self.center[1] + self.altura) - origin[1]) / direccion[1]
+        if self.capasVerif(origin, direccion, t_superior):
+            hit = sum(origin, mul(direccion, t_superior))
             normal = norm(sub(self.center,hit))
 
             return Intersect(distance = t_superior,
                              point=hit,
                              normal=normal,
-                             texCoords = None,
-                             sceneObject = self)
+                             Coordenadas = None,
+                             scene = self)
 
         return None
         
